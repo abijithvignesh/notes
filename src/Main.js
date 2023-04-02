0878579df2417b97cc13023aec314040f33044b9
abijithvignesh <60066300+abijithvignesh@users.noreply.github.com>
2023-04-02 11:51:49 +0530

    First commit for note app

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+const Main = () => {
+  return (
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <input type="text" id="title" autoFocus />
+      </div>
+      <div className="='app-main-note-preview"></div>
+    </div>
+  );
+};
+
+export default Main;
